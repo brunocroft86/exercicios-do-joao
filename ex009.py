fd305687a1bb18fe75d56b18cff4db7bc8399a2e
brunoluck <97357628+brunocroft86@users.noreply.github.com>
2023-04-09 17:07:52 -0300

    first

--- ex009.py
+++ ex009.py
@@ -1,8 +1,25 @@
 #9) Escreva um algoritmo para ler o salário mensal atual de um funcionário e o percentual de reajuste.
 #Calcular e escrever o valor do novo salário.
 
-salarioatual = float(input('Digite o salário atual: '))
-percreajuste = float(input('Digite o reajuste: '))
-novosalario = (salarioatual * percreajuste)/100
-print(f'O novo salário é R${novosalario + salarioatual:.2f}')
+# salarioatual = float(input('Digite o salário atual: '))
+# percreajuste = float(input('Digite o reajuste: '))
+# novosalario = (salarioatual * percreajuste)/100
+# print(f'O novo salário é de R${novosalario + salarioatual:.2f}')
+
+def aumentaSalario(salarioAtual,percReajuste):
+    aumento = (salarioAtual * percReajuste) / 100
+    novoSalario = aumento + salarioAtual
+    return novoSalario
+salarioAtual = float(input('Digite o salário atual:R$ '))
+percReajuste = float(input('Digite o reajuste:R$ '))
+resultado = aumentaSalario(salarioAtual,percReajuste)
+print(f'o seu novo salário é de R${resultado:.2f}')
+
+
+
+
+
+
+
+
 
